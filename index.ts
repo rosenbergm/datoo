a7475854a3a58d953cc4cf864ae325960f0153b3
Martin Rosenberg <nitram.rosenberg@gmail.com>
2022-01-09 14:27:35 +0100

    ✨ First primitive version

--- index.ts
+++ index.ts
@@ -1,3 +1,31 @@
-export const ahoj = (a: number, b: number) => {
-  return a + b
+import {useState, useEffect} from 'react'
+
+interface GetOptions {
+  fetchOptions: RequestInit
+  runAutomatically: boolean
 }
+
+export const useGetData = <T,>(url: string, decoder: (data: unknown) => (T | null), options?: GetOptions)  => {
+  const [isLoading, setLoading] = useState(options?.runAutomatically ?? true)
+  const [data, setData] = useState<T | null>(null)
+
+  const setAndDecode = (data: unknown) => setData(decoder(data))
+
+  useEffect(() => {
+    setLoading(true)
+
+    fetch(url, {
+      method: 'GET',
+      ...options?.fetchOptions
+    })
+      .then(setAndDecode)
+      .catch(() => setData(null))
+      .finally(() => setLoading(false))
+  }, [])
+
+  return {
+    isLoading,
+    data
+  }
+}
+
